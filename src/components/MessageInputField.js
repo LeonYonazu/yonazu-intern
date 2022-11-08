2ed0cb15daef18a80fac8a004a2cbb143e8fb44c
JulianYona <66995519+JulianYona@users.noreply.github.com>
2022-11-09 01:22:03 +0900

    グリッドレイアウトの適応

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Avatar, Grid } from '@material-ui/core';
+
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '26px',
   },
 });
 
@@ -11,7 +14,14 @@ const MessageInputField = () =>{
   const classes = useStyles();
   return (
   <div className={classes.root}>
-    MessageInputField
+    <Grid container>
+      <Grid xs={1}>
+        <Avatar/>
+      </Grid>
+      <Grid xs={10}>入力</Grid>
+      <Grid xs={1}>ボタン</Grid>
+    </Grid>
+
   </div> 
   )};
 
