b9d22e19403279fd2b575147f03a4c0034aed99c
JulianYona <66995519+JulianYona@users.noreply.github.com>
2022-11-08 16:52:21 +0900

    config.jsonを作成した

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import SignIn from './SignIn';
 import Main from './Main';
+import config from '../config.json';
 
 export default () => {
   const [name, setName] = useState('');
-  console.log({name});
 
-  if (name === ''){
+//SignInを表示させるか、Mainを表示させるか。開発に際しいちいちSignInが表示されると面倒なのでconfig.jsonを作成した。
+  if (config.signInEnabled && name === ''){
     return <SignIn setName={setName}/>
   } else{
     return<Main name = {name}/>
