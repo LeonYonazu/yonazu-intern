6316e940366b14c79c2a3cbc02a0ced0457c681c
JulianYona <66995519+JulianYona@users.noreply.github.com>
2022-11-10 15:41:01 +0900

    ハードコーディングを削除

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAEbx9k_Lz5eqQ4BtKNWD8_JxEGG97a0IA",
-  authDomain: "yonazu-intern.firebaseapp.com",
-  databaseURL: "https://yonazu-intern-default-rtdb.firebaseio.com",
-  projectId: "yonazu-intern",
-  storageBucket: "yonazu-intern.appspot.com",
-  messagingSenderId: "6346044122",
-  appId: "1:6346044122:web:c80006537dc454eb412be3"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 
