511ddd857e4594e8ffa84ee30fbd1f4cac4a78d7
JulianYona <66995519+JulianYona@users.noreply.github.com>
2022-11-11 18:29:14 +0900

    メッセージの表示

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react';
+import {List} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
+import MessageItem from './MessageItem';
 import { messagesRef } from '../firebase';
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
+    overflow: 'auto',
+    width: '100%',
   },
 });
 
@@ -30,9 +33,13 @@ const MessageList = () =>{
     });
   }, []);
   return (
-  <div className={classes.root}>
-    MessageList
-  </div> 
+  <List className={classes.root}>
+    {
+      messages.map(({key,name,text}) =>{
+        return <MessageItem key={key} name={name} text={text}>item</MessageItem>
+      })}
+
+  </List> 
   )};
 
 export default MessageList;
